450fa30dd5a84477d9d5946a68b8e912387259a0
Emily <emily_kasbohm@brown.edu>
2016-05-11 21:05:31 -0400

    Removing commented-out code.

--- project_v1/public/spotify-connect.js
+++ project_v1/public/spotify-connect.js
@@ -45,9 +45,9 @@ Aquires Login tokens from spotify and gets data
  error = params.error;
 
 
-  var audio = new Audio();
-  var audioTracks = {};
-  var audioPlay = -1;
+ var audio = new Audio();
+ var audioTracks = {};
+ var audioPlay = -1;
 
 
  if (error) {
@@ -70,69 +70,20 @@ Aquires Login tokens from spotify and gets data
 
           var icon = d3.select("#icon");
           console.log(icon);
-            icon.transition("shrink")
-            .delay(0)
-            .duration(1200)
-            .style("width", "50px")
-            .style("height", "50px");
+          icon.transition("shrink")
+          .delay(0)
+          .duration(1200)
+          .style("width", "50px")
+          .style("height", "50px");
 
           $('#login').hide();
           $('#loggedin').show();
           $('#rec-artists-title').hide();
 
-          /*document.getElementById('obtain-new-token').addEventListener('click', function() {
-            $.ajax({
-              url: '/refresh_token',
-              data: {
-                'refresh_token': refresh_token
-              }
-            }).done(function(data) {
-              access_token = data.access_token;
-              oauthPlaceholder.innerHTML = oauthTemplate({
-                access_token: access_token,
-                refresh_token: refresh_token
-              });
-            });
-          }, false);*/
-
         }
       });
-      /*$.ajax({
-          url: 'https://api.spotify.com/v1/me/playlists',
-          headers: {
-            'Authorization': 'Bearer ' + access_token
-          },
-          success: function(response) {
-            userProfilePlaceholder.innerHTML = userProfileTemplate(response);
 
-            console.log(response);
-            for (var i = 0; i < response.items.length; i++) {
-              musicList.innerHTML += response.items[i].name + "<br>";
-              $.ajax({
-                url: response.items[i]['tracks']['href'],
-                headers: {
-                  'Authorization': 'Bearer ' + access_token
-                },
-                success: function(response) {
-                  userProfilePlaceholder.innerHTML = userProfileTemplate(response);
-
-                  console.log(response);
-                  for (var i = 0; i < response.items.length; i++) {
-                    musicList.innerHTML += "--" + response.items[i].track.name + "<br>";
-                    
-                  }
-
-                  $('#login').hide();
-                  $('#loggedin').show();
-                }
-              });
-            }
-
-            $('#login').hide();
-            $('#loggedin').show();
-          }
-        });*/
-      } else {
+    } else {
         // render initial screen
         $('#login').show();
         $('#loggedin').hide();
@@ -220,36 +171,34 @@ Aquires Login tokens from spotify and gets data
           var defs = svg.append('svg:defs');
 
 
-        defs.append("svg:pattern")
-            .attr("id", "play")
-            .attr("width", 40)
-            .attr("height", 40)
-            .attr("x", 0)
-            .attr("y", 0)
-            .append("svg:image")
-            .attr("xlink:href", "http://plainicon.com/dboard/userprod/2803_dd580/prod_thumb/plainicon.com-40678-512px.png")
-            .attr("width", 40)
-            .attr("height", 40)
-            .attr("x", 0)
-            .attr("y", 0);
+          defs.append("svg:pattern")
+          .attr("id", "play")
+          .attr("width", 40)
+          .attr("height", 40)
+          .attr("x", 0)
+          .attr("y", 0)
+          .append("svg:image")
+          .attr("xlink:href", "http://plainicon.com/dboard/userprod/2803_dd580/prod_thumb/plainicon.com-40678-512px.png")
+          .attr("width", 40)
+          .attr("height", 40)
+          .attr("x", 0)
+          .attr("y", 0);
 
-        defs.append("svg:pattern")
-            .attr("id", "stop")
-            .attr("width", 40)
-            .attr("height", 40)
-            .attr("x", 0)
-            .attr("y", 0)
-            .append("svg:image")
-            .attr("xlink:href", "https://image.freepik.com/free-icon/stop-button_318-11112.jpg")
-            .attr("width", 40)
-            .attr("height", 40)
-            .attr("x", 0)
-            .attr("y", 0);
-
-
-        for (var i = 0; i < rec_artists.length; i++) {
+          defs.append("svg:pattern")
+          .attr("id", "stop")
+          .attr("width", 40)
+          .attr("height", 40)
+          .attr("x", 0)
+          .attr("y", 0)
+          .append("svg:image")
+          .attr("xlink:href", "https://image.freepik.com/free-icon/stop-button_318-11112.jpg")
+          .attr("width", 40)
+          .attr("height", 40)
+          .attr("x", 0)
+          .attr("y", 0);
 
 
+          for (var i = 0; i < rec_artists.length; i++) {
             var artist = rec_artists[i];
             var track = toptrackdict[artist.id];
             var imageurl = artist.images[2].url;
@@ -268,9 +217,7 @@ Aquires Login tokens from spotify and gets data
             .attr("x", 0)
             .attr("y", 0);
 
-          var circ = svg.append("circle")
-            //.attr("class", artist.id)
-
+            var circ = svg.append("circle")
             .attr("r", 100)
             .attr("cx", (i + 1) * 250)
             .attr("cy", 200+100)
@@ -280,7 +227,7 @@ Aquires Login tokens from spotify and gets data
             .style("fill", "#666")
             .style("fill", "url(#"+artist.id+")");
 
-          var circ2 = svg.append("circle")
+            var circ2 = svg.append("circle")
             .attr("id", artist.id+"_play")
             .attr("class", artist.id)
             .attr("r", 20)
@@ -290,30 +237,30 @@ Aquires Login tokens from spotify and gets data
             .style("stroke-width", 0.25)
             .style("fill","url(#play)")
             .on("mouseover", function(){
-               d3.select(this)
-                   .style("stroke-width", 0.75);
-            })
+             d3.select(this)
+             .style("stroke-width", 0.75);
+           })
             .on("mouseout", function(){ 
-               d3.select(this)
-                   .style("stroke-width", 0.25);
-            })
+             d3.select(this)
+             .style("stroke-width", 0.25);
+           })
             .on("click", function(){
 
-                var curr = d3.select(this);
-                console.log(curr.attr("class"));
-                console.log(audioTracks[curr.attr("class")]);
-                if (audioPlay != -1 && audioPlay.attr("id") == curr.attr("id")) {
-                  console.log("my song is playing!");
-                  audio.pause();
-                  audioPlay = -1;
-                  curr.style("fill","url(#play)");
-                } else {
-                  if (audioPlay != -1) {
-                    audioPlay
-                      .style("fill","url(#play)");
-                  }
+              var curr = d3.select(this);
+              console.log(curr.attr("class"));
+              console.log(audioTracks[curr.attr("class")]);
+              if (audioPlay != -1 && audioPlay.attr("id") == curr.attr("id")) {
+                console.log("my song is playing!");
+                audio.pause();
+                audioPlay = -1;
+                curr.style("fill","url(#play)");
+              } else {
+                if (audioPlay != -1) {
+                  audioPlay
+                  .style("fill","url(#play)");
+                }
 
-                  console.log("my song is being loaded!");
+                console.log("my song is being loaded!");
                   //audio.pause();
                   audio.src = audioTracks[curr.attr("class")];
                   audio.play();
@@ -323,120 +270,101 @@ Aquires Login tokens from spotify and gets data
                 }
                 
 
-            });
-          
+              });
 
-            /*var circ2 = svg.append("circle")
-            .attr("r", 40)
-            .attr("cx", (i + 1) * 200 + 100)
-            .attr("cy", 200+100+140)
-            .style("stroke", "black")  
-            .style("stroke-width", 0.25);
-            //.style("fill", "#666");
-            //.style("fill", "url(#"+artist.id+")");*/
+var artist_stem = "https://api.spotify.com/v1/artists/";
+var artist = artist_stem + rec_artists[i].id;
 
+var to_split = rec_artists[i].name
+if (to_split.substring(0, 22) == "Original Broadway Cast")
+  to_split = "OBC " + to_split.substring(23);
 
-          /*var circ = svg.append("image")
-            .attr("xlink:href", imageurl)
-            .attr("x", (i + 1) * 300 - 160 - 100)
-            .attr("y", 300 - 100)
-            .attr("width", 200)
-            .attr("height", 200)
-            .style("border-radius","5em");*/
-
-            var artist_stem = "https://api.spotify.com/v1/artists/";
-            var artist = artist_stem + rec_artists[i].id;
-
-            var to_split = rec_artists[i].name
-            if (to_split.substring(0, 22) == "Original Broadway Cast")
-              to_split = "OBC " + to_split.substring(23);
-
-            var split_name = to_split.split(" ");
+var split_name = to_split.split(" ");
 
 
-          for (var k = 0; k < split_name.length; k++) {
-            svg.append("text")
-              .attr("x", (i + 1) * 250)
-              .attr("y", 280 + 30 * k)
-              .attr("text-anchor", "middle")
-              .text(split_name[k])
-              .style("fill", "white")
-              .style("font-size", "24px")
-              .style("font-weight", "bold")
-              .style("text-shadow", "0 0 20px #000");
-            }
-          }
+for (var k = 0; k < split_name.length; k++) {
+  svg.append("text")
+  .attr("x", (i + 1) * 250)
+  .attr("y", 280 + 30 * k)
+  .attr("text-anchor", "middle")
+  .text(split_name[k])
+  .style("fill", "white")
+  .style("font-size", "24px")
+  .style("font-weight", "bold")
+  .style("text-shadow", "0 0 20px #000");
+}
+}
 
-          var pos_index = 0;
-          var found = false;
-          for (var i = 0; i < top_artists_info.length; i++) {
-            var top_artist = top_artists_info[i];
-            found = false;
-            for (var artistidx in rec_artists) {
-              var recartist = rec_artists[artistidx];
-              var recid = recartist.id;
+var pos_index = 0;
+var found = false;
+for (var i = 0; i < top_artists_info.length; i++) {
+  var top_artist = top_artists_info[i];
+  found = false;
+  for (var artistidx in rec_artists) {
+    var recartist = rec_artists[artistidx];
+    var recid = recartist.id;
+    
+    for (var j = 0; j < recommending[recid].length; j++){
       
-              for (var j = 0; j < recommending[recid].length; j++){
-                
-                if (recommending[recid][j].id == top_artist.id) {
+      if (recommending[recid][j].id == top_artist.id) {
 
-                  var circ = svg.append("circle")
-                  .attr("r", 50)
-                  .attr("cx", dim/2 + (((pos_index %2)*2 -1) * (Math.floor((pos_index+1)/2) * 2) * 55))
-                  .attr("cy", 80)
-                  .attr("id", top_artists[i].split(' ').join('').replace(".",""))
-                  .style("stroke", "#00c844")
-                  .style("fill", "#009633");
+        var circ = svg.append("circle")
+        .attr("r", 50)
+        .attr("cx", dim/2 + (((pos_index %2)*2 -1) * (Math.floor((pos_index+1)/2) * 2) * 55))
+        .attr("cy", 80)
+        .attr("id", top_artists[i].split(' ').join('').replace(".",""))
+        .style("stroke", "#00c844")
+        .style("fill", "#009633");
 
-                  var to_split = top_artists[i]
-                  if (to_split.substring(0, 22) == "Original Broadway Cast")
-                    to_split = "OBC " + to_split.substring(23);
+        var to_split = top_artists[i]
+        if (to_split.substring(0, 22) == "Original Broadway Cast")
+          to_split = "OBC " + to_split.substring(23);
 
-                  var split_name = to_split.split(" ");
+        var split_name = to_split.split(" ");
 
-                  for (var k = 0; k < split_name.length; k++) {
-                    svg.append("text")
-                    .attr("x", dim/2 + (((pos_index %2)*2 -1) * (Math.floor((pos_index+1)/2) * 2) * 55))
-                    .attr("y", 70 + 18 * k)
-                    .attr("text-anchor", "middle")
-                    .text(split_name[k])
-                    .style("fill", "white");
-                  }
-                  pos_index++;
-                  found = true;
-                  break;
-                }
-              }
-              if (found) {
-                break;
-              }
-            }
-          }
-          for (var key in recommending) {
-            for (var i = 0; i < rec_artists.length; i++) {
-              if (key == rec_artists[i].id) {
-                for (var j = 0; j < recommending[key].length; j++) {
-                  svg.append('line')
-                  .attr("stroke", "black")
-                  .attr("stroke-width", "2px")
-                  .attr("x1", d3.select("#"+rec_artists[i].name.split(' ').join('').replace(".", '')).attr("cx"))
-                  .attr("y1", d3.select("#"+rec_artists[i].name.split(' ').join('').replace(".", '')).attr("cy") - 100)
-                  .attr("x2", d3.select("#"+recommending[key][j].name.split(' ').join('').replace(".", '')).attr("cx"))
-                  .attr("y2", 130)
-                  .on("mouseover", function() {
-                    d3.select(this)
-                    .style("stroke", "orange");        
-                  })
-                  .on("mouseout", function() {
-                    d3.select(this)
-                    .style("stroke", "black")
-                  })
-                }
-              }
-            }
-          }
+        for (var k = 0; k < split_name.length; k++) {
+          svg.append("text")
+          .attr("x", dim/2 + (((pos_index %2)*2 -1) * (Math.floor((pos_index+1)/2) * 2) * 55))
+          .attr("y", 70 + 18 * k)
+          .attr("text-anchor", "middle")
+          .text(split_name[k])
+          .style("fill", "white");
+        }
+        pos_index++;
+        found = true;
+        break;
+      }
+    }
+    if (found) {
+      break;
+    }
+  }
+}
+for (var key in recommending) {
+  for (var i = 0; i < rec_artists.length; i++) {
+    if (key == rec_artists[i].id) {
+      for (var j = 0; j < recommending[key].length; j++) {
+        svg.append('line')
+        .attr("stroke", "black")
+        .attr("stroke-width", "2px")
+        .attr("x1", d3.select("#"+rec_artists[i].name.split(' ').join('').replace(".", '')).attr("cx"))
+        .attr("y1", d3.select("#"+rec_artists[i].name.split(' ').join('').replace(".", '')).attr("cy") - 100)
+        .attr("x2", d3.select("#"+recommending[key][j].name.split(' ').join('').replace(".", '')).attr("cx"))
+        .attr("y2", 130)
+        .on("mouseover", function() {
+          d3.select(this)
+          .style("stroke", "orange");        
+        })
+        .on("mouseout", function() {
+          d3.select(this)
+          .style("stroke", "black")
+        })
+      }
+    }
+  }
+}
 
-        });
+});
 }, false);
 
 
